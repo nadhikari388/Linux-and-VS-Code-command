91391fa9c3cb5c97230222f9f284474ceeb9c260
Narayan Adhikari <nadhikari388@gmail.com>
2025-04-14 18:57:44 +0200

    Definition of reference and pointer

---
+++ Call_by_reference.cpp
@@ -0,0 +1,2 @@
+// Reference must be initialized. Reference always needs to be initialized && a reference cannot be reseated i.e always refers to the same object.
+// A pointer is a type that restores an adress and we can modify it at runtime. if we have to access the content of that address we need to dereference it.
